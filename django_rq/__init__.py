58d812548694f0666893e8998b412978ce48e78b
Selwin Ong <selwin.ong@gmail.com>
2012-06-08 22:29:24 +0700

    Bump version

--- django_rq/__init__.py
+++ django_rq/__init__.py
@@ -1,1 +1,3 @@
+VERSION = (0, 1, 1)
+
 from .queues import get_connection, get_queue, enqueue
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django-rq',
-    version='0.1.0',
+    version='0.1.1',
     author='Selwin Ong',
     author_email='selwin.ong@gmail.com',
     packages=['django_rq'],
