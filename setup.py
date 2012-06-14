ab71ec196dbd864fb5232ccd449d20b9d48105bc
Selwin Ong <selwin.ong@gmail.com>
2012-06-14 21:47:01 +0700

    Changed version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-rq',
-    version='0.1.1',
+    version='0.1.2',
     author='Selwin Ong',
     author_email='selwin.ong@gmail.com',
     packages=['django_rq'],
