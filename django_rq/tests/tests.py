efaa43075d8cb1f30243b74ac6eb0ee550d28ea9
jean-philippe serafin <serafinjp@gmail.com>
2012-08-22 12:28:46 +0200

    added test.settings module

--- django_rq/tests/tests.py
+++ django_rq/tests/tests.py
@@ -3,11 +3,12 @@ from django.core.management import call_command
 from django.test import TestCase
 from django.utils.unittest import skipIf
 from django.test.utils import override_settings
+from django.conf import settings
 
 from rq.job import Job
 
-from .management.commands.rqworker import get_queues
-from .queues import get_connection, get_queue
+from django_rq.management.commands.rqworker import get_queues
+from django_rq.queues import get_connection, get_queue
 
 try:
     from rq_scheduler import Scheduler
@@ -17,51 +18,33 @@ except ImportError:
     RQ_SCHEDULER_INSTALLED = False
 
 
-TEST_QUEUES = {
-    'default': {
-        'HOST': 'localhost',
-        'PORT': 6379,
-        'DB': 0,
-    },
-    'test': {
-        'HOST': 'localhost',
-        'PORT': 1,
-        'DB': 1,
-    },
-    'test2': {
-        'HOST': 'localhost',
-        'PORT': 1,
-        'DB': 1,
-    },
-}
+QUEUES = settings.RQ_QUEUES
 
 
 class DjangoRQTest(TestCase):
 
-    @override_settings(RQ_QUEUES=TEST_QUEUES)
     def test_get_connection(self):
         """
-        Test that get_connection returns the right connection based on 
+        Test that get_connection returns the right connection based on
         settings.RQ_QUEUES
         """
-        config = TEST_QUEUES['default']
+        config = QUEUES['default']
         connection = get_connection()
         connection_kwargs = connection.connection_pool.connection_kwargs
         self.assertEqual(connection_kwargs['host'], config['HOST'])
         self.assertEqual(connection_kwargs['port'], config['PORT'])
         self.assertEqual(connection_kwargs['db'], config['DB'])
 
-        config = TEST_QUEUES['test']
+        config = QUEUES['test']
         connection = get_connection('test')
         connection_kwargs = connection.connection_pool.connection_kwargs
         self.assertEqual(connection_kwargs['host'], config['HOST'])
         self.assertEqual(connection_kwargs['port'], config['PORT'])
         self.assertEqual(connection_kwargs['db'], config['DB'])
 
-    @override_settings(RQ_QUEUES=TEST_QUEUES)
     def test_get_queue(self):
         # Test that get_queue use the right parameters for its connection
-        config = TEST_QUEUES['default']
+        config = QUEUES['default']
         queue = get_queue('default')
         connection_kwargs = queue.connection.connection_pool.connection_kwargs
         self.assertEqual(queue.name, 'default')
@@ -69,7 +52,7 @@ class DjangoRQTest(TestCase):
         self.assertEqual(connection_kwargs['port'], config['PORT'])
         self.assertEqual(connection_kwargs['db'], config['DB'])
 
-        config = TEST_QUEUES['test']
+        config = QUEUES['test']
         queue = get_queue('test')
         connection_kwargs = queue.connection.connection_pool.connection_kwargs
         self.assertEqual(queue.name, 'test')
@@ -82,34 +65,32 @@ class DjangoRQTest(TestCase):
         # Raise an exception if RQ_QUEUES is not defined
         self.assertRaises(ImproperlyConfigured, get_connection)
 
-    @override_settings(RQ_QUEUES=TEST_QUEUES)
-    def test_get_queues(self):        
+    def test_get_queues(self):
         # Getting queues with the same redis connection is ok
         self.assertEqual(get_queues('test', 'test2'), [get_queue('test'), get_queue('test2')])
         # Getting queues with different connections raises an exception
         self.assertRaises(ValueError, get_queues, 'default', 'test')
 
-    @override_settings(RQ_QUEUES=TEST_QUEUES)
     @skipIf(RQ_SCHEDULER_INSTALLED is False, 'RQ Scheduler not installed')
-    def test_get_scheduler(self):        
+    def test_get_scheduler(self):
         """
         Ensure get_scheduler creates a scheduler instance with the right
         connection params.
         """
-        
-        config = TEST_QUEUES['default']
+
+        config = QUEUES['default']
         scheduler = get_scheduler()
         connection_kwargs = scheduler.connection.connection_pool.connection_kwargs
         self.assertEqual(scheduler.queue_name, 'default')
         self.assertEqual(connection_kwargs['host'], config['HOST'])
         self.assertEqual(connection_kwargs['port'], config['PORT'])
-        self.assertEqual(connection_kwargs['db'], config['DB'])        
+        self.assertEqual(connection_kwargs['db'], config['DB'])
 
-        config = TEST_QUEUES['test']
+        config = QUEUES['test']
         scheduler = get_scheduler('test')
         connection_kwargs = scheduler.connection.connection_pool.connection_kwargs
         self.assertEqual(scheduler.queue_name, 'test')
         self.assertEqual(connection_kwargs['host'], config['HOST'])
         self.assertEqual(connection_kwargs['port'], config['PORT'])
-        self.assertEqual(connection_kwargs['db'], config['DB'])        
+        self.assertEqual(connection_kwargs['db'], config['DB'])
 
