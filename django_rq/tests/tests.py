d4a355bc4366f91127a2827a87817c50ac0c6179
jean-philippe serafin <serafinjp@gmail.com>
2012-08-22 15:40:41 +0200

    unit tests are unitary

--- django_rq/tests/tests.py
+++ django_rq/tests/tests.py
@@ -7,8 +7,7 @@ from django.conf import settings
 
 from rq.job import Job
 
-from django_rq.management.commands.rqworker import get_queues
-from django_rq.queues import get_connection, get_queue
+from django_rq.queues import get_connection, get_queue, get_queues
 
 try:
     from rq_scheduler import Scheduler
@@ -21,12 +20,11 @@ except ImportError:
 QUEUES = settings.RQ_QUEUES
 
 
-class DjangoRQTest(TestCase):
+class QueuesTest(TestCase):
 
-    def test_get_connection(self):
-        """
-        Test that get_connection returns the right connection based on
-        settings.RQ_QUEUES
+    def test_get_connection_default(self):
+        """Test that get_connection returns the right connection based for
+        `defaut` queue.
         """
         config = QUEUES['default']
         connection = get_connection()
@@ -35,6 +33,10 @@ class DjangoRQTest(TestCase):
         self.assertEqual(connection_kwargs['port'], config['PORT'])
         self.assertEqual(connection_kwargs['db'], config['DB'])
 
+    def test_get_connection_test(self):
+        """Test that get_connection returns the right connection based for
+        `test` queue.
+        """
         config = QUEUES['test']
         connection = get_connection('test')
         connection_kwargs = connection.connection_pool.connection_kwargs
@@ -42,8 +44,10 @@ class DjangoRQTest(TestCase):
         self.assertEqual(connection_kwargs['port'], config['PORT'])
         self.assertEqual(connection_kwargs['db'], config['DB'])
 
-    def test_get_queue(self):
-        # Test that get_queue use the right parameters for its connection
+    def test_get_queue_default(self):
+        """Test that get_queue use the right parameters for `default`
+        connection.
+        """
         config = QUEUES['default']
         queue = get_queue('default')
         connection_kwargs = queue.connection.connection_pool.connection_kwargs
@@ -52,6 +56,10 @@ class DjangoRQTest(TestCase):
         self.assertEqual(connection_kwargs['port'], config['PORT'])
         self.assertEqual(connection_kwargs['db'], config['DB'])
 
+    def test_get_queue_test(self):
+        """Test that get_queue use the right parameters for `test`
+        connection.
+        """
         config = QUEUES['test']
         queue = get_queue('test')
         connection_kwargs = queue.connection.connection_pool.connection_kwargs
@@ -65,19 +73,22 @@ class DjangoRQTest(TestCase):
         # Raise an exception if RQ_QUEUES is not defined
         self.assertRaises(ImproperlyConfigured, get_connection)
 
-    def test_get_queues(self):
-        # Getting queues with the same redis connection is ok
+    def test_get_queues_same_connection(self):
+        """Checks that getting queues with the same redis connection is ok.
+        """
         self.assertEqual(get_queues('test', 'test2'), [get_queue('test'), get_queue('test2')])
-        # Getting queues with different connections raises an exception
+
+    def test_get_queues_different_connections(self):
+        """Checks that getting queues with different redis connections raise
+        an exception.
+        """
         self.assertRaises(ValueError, get_queues, 'default', 'test')
 
     @skipIf(RQ_SCHEDULER_INSTALLED is False, 'RQ Scheduler not installed')
     def test_get_scheduler(self):
+        """Ensure get_scheduler creates a scheduler instance with the right
+        connection params for `default` queue.
         """
-        Ensure get_scheduler creates a scheduler instance with the right
-        connection params.
-        """
-
         config = QUEUES['default']
         scheduler = get_scheduler()
         connection_kwargs = scheduler.connection.connection_pool.connection_kwargs
@@ -86,6 +97,11 @@ class DjangoRQTest(TestCase):
         self.assertEqual(connection_kwargs['port'], config['PORT'])
         self.assertEqual(connection_kwargs['db'], config['DB'])
 
+    @skipIf(RQ_SCHEDULER_INSTALLED is False, 'RQ Scheduler not installed')
+    def test_get_scheduler(self):
+        """Ensure get_scheduler creates a scheduler instance with the right
+        connection params for `test` queue.
+        """
         config = QUEUES['test']
         scheduler = get_scheduler('test')
         connection_kwargs = scheduler.connection.connection_pool.connection_kwargs
