24c70111d25a8fa9b663612be3cb46f8d4a68966
Selwin Ong <selwin.ong@gmail.com>
2013-01-21 12:59:43 +0700

    Bumped version to 0.4.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-rq',
-    version='0.4.2',
+    version='0.4.3',
     author='Selwin Ong',
     author_email='selwin.ong@gmail.com',
     packages=['django_rq'],
