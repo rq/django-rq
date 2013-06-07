2c6e144501d9eeb00618d15bcb3da1fcd5141398
Selwin Ong <selwin.ong@gmail.com>
2013-06-07 11:27:39 +0700

    Bump version to 0.5.1.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-rq',
-    version='0.5.0',
+    version='0.5.1',
     author='Selwin Ong',
     author_email='selwin.ong@gmail.com',
     packages=['django_rq'],
