3ebcbafdaff070b2157b5fb065da9a950ba58d1c
Anthony Lalande <anthony@5by.com>
2015-07-10 14:43:32 -0700

    Sort the queues by name, so that they are deterministic across instances of `django_rq`.

--- django_rq/settings.py
+++ django_rq/settings.py
@@ -13,7 +13,7 @@ BURST = getattr(settings, 'RQ_BURST', False)
 
 # All queues in list format so we can get them by index, includes failed queues
 QUEUES_LIST = []
-for key, value in QUEUES.items():
+for key, value in sorted(QUEUES.items()):
     QUEUES_LIST.append({'name': key, 'connection_config': value})
 for config in get_unique_connection_configs():
     QUEUES_LIST.append({'name': 'failed', 'connection_config': config})
