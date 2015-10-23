6a9de501ec94f786584c22eb1e87e4801d45417a
Douglas Soares de Andrade <contato@douglasandrade.com>
2015-10-22 23:39:45 -0200

    Remove 1.9 deprecation warnings in urls.py

--- django_rq/urls.py
+++ django_rq/urls.py
@@ -1,21 +1,24 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from .views import stats, jobs, finished_jobs, started_jobs, deferred_jobs
+from .views import clear_queue, job_detail, delete_job, actions, requeue_job_view
 
-urlpatterns = patterns('django_rq.views',
-    url(r'^$', 'stats', name='rq_home'),
-    url(r'^queues/(?P<queue_index>[\d]+)/$', 'jobs', name='rq_jobs'),
+
+urlpatterns = [
+    url(r'^$', stats, name='rq_home'),
+    url(r'^queues/(?P<queue_index>[\d]+)/$', jobs, name='rq_jobs'),
     url(r'^queues/(?P<queue_index>[\d]+)/finished/$',
-        'finished_jobs', name='rq_finished_jobs'),
+        finished_jobs, name='rq_finished_jobs'),
     url(r'^queues/(?P<queue_index>[\d]+)/started/$',
-        'started_jobs', name='rq_started_jobs'),
+        started_jobs, name='rq_started_jobs'),
     url(r'^queues/(?P<queue_index>[\d]+)/deferred/$',
-        'deferred_jobs', name='rq_deferred_jobs'),
-    url(r'^queues/(?P<queue_index>[\d]+)/empty/$', 'clear_queue', name='rq_clear'),
-    url(r'^queues/(?P<queue_index>[\d]+)/(?P<job_id>[-\w]+)/$', 'job_detail',
+        deferred_jobs, name='rq_deferred_jobs'),
+    url(r'^queues/(?P<queue_index>[\d]+)/empty/$', clear_queue, name='rq_clear'),
+    url(r'^queues/(?P<queue_index>[\d]+)/(?P<job_id>[-\w]+)/$', job_detail,
         name='rq_job_detail'),
     url(r'^queues/(?P<queue_index>[\d]+)/(?P<job_id>[-\w]+)/delete/$',
-        'delete_job', name='rq_delete_job'),
+        delete_job, name='rq_delete_job'),
     url(r'^queues/actions/(?P<queue_index>[\d]+)/$',
-        'actions', name='rq_actions'),
+        actions, name='rq_actions'),
     url(r'^queues/(?P<queue_index>[\d]+)/(?P<job_id>[-\w]+)/requeue/$',
-        'requeue_job_view', name='rq_requeue_job'),
-)
+        requeue_job_view, name='rq_requeue_job'),
+]
