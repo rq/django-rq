bcf410e2662ec817f60a9128639b4bcb4809b850
Kirill Goncharov <kdgoncharov@yandex.ru>
2015-11-30 17:30:14 +0300

    Allow exception handlers to be defined in settings

--- django_rq/workers.py
+++ django_rq/workers.py
@@ -1,6 +1,24 @@
+from django.conf import settings
+
 from rq import Worker
+from rq.utils import import_attribute
 
 from .queues import get_queues
+
+
+def get_exception_handlers():
+    """
+    Custom exception handlers could be defined in settings.py:
+    RQ = {
+        'EXCEPTION_HANDLERS': ['path.to.handler'],
+    }
+    """
+    RQ = getattr(settings, 'RQ', {})
+    exception_handlers = []
+    for path in RQ.get('EXCEPTION_HANDLERS', []):
+        handler = import_attribute(path)
+        exception_handlers.append(handler)
+    return exception_handlers
 
 
 def get_worker(*queue_names):
@@ -8,4 +26,6 @@ def get_worker(*queue_names):
     Returns a RQ worker for all queues or specified ones.
     """
     queues = get_queues(*queue_names)
-    return Worker(queues, connection=queues[0].connection)
+    return Worker(queues,
+                  connection=queues[0].connection,
+                  exception_handlers=get_exception_handlers() or None)
