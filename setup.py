60a1ae075a5b4ed63ec8c291d6ba18e3762cfd59
Douglas Soares de Andrade <contato@douglasandrade.com>
2015-12-10 21:44:13 -0200

    Changing version to 0.9.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-rq',
-    version='0.8.0',
+    version='0.9.0',
     author='Selwin Ong',
     author_email='selwin.ong@gmail.com',
     packages=['django_rq'],
