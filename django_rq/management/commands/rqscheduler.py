2c3a17c87f29a973c9ef9daf5d56ea0bb81dd54d
sbussetti <steve.bussetti@gmail.com>
2016-01-18 15:35:42 -0500

    makes queue name configurable for rqscheduler

--- django_rq/management/commands/rqscheduler.py
+++ django_rq/management/commands/rqscheduler.py
@@ -19,8 +19,16 @@ class Command(BaseCommand):
             help="How often the scheduler checks for new jobs to add to the "
                  "queue (in seconds).",
         ),
+        make_option(
+            '--queue',
+            type=str,
+            dest='queue',
+            default='default',
+            help="Name of the queue used for scheduling.",
+        ),
     )
 
-    def handle(self, queue='default', *args, **options):
-        scheduler = get_scheduler(name=queue, interval=options.get('interval'))
+    def handle(self, *args, **options):
+        scheduler = get_scheduler(
+            name=options.get('queue'), interval=options.get('interval'))
         scheduler.run()
