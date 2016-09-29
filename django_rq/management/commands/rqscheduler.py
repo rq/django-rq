26fffd107e2071d6bb0529c3c05f08c6688a3838
Vincenzo Demasi <vindemasi@gmail.com>
2016-09-29 15:09:43 +0200

    added a pid file argument to rqscheduler command

--- django_rq/management/commands/rqscheduler.py
+++ django_rq/management/commands/rqscheduler.py
@@ -1,3 +1,4 @@
+import os
 from distutils.version import LooseVersion
 
 from django.core.management.base import BaseCommand
@@ -13,6 +14,8 @@ class Command(BaseCommand):
     args = '<queue>'
 
     def add_arguments(self, parser):
+        parser.add_argument('--pid', action='store', dest='pid',
+                            default=None, help='PID file to write the scheduler`s pid into')
         parser.add_argument('--interval', '-i', type=int, dest='interval',
                             default=60, help="""How often the scheduler checks for new jobs to add to the
                             queue (in seconds).""")
@@ -23,6 +26,11 @@ class Command(BaseCommand):
             parser.add_argument('args', nargs='*')
 
     def handle(self, *args, **options):
+        pid = options.get('pid')
+        if pid:
+            with open(os.path.expanduser(pid), "w") as fp:
+                fp.write(str(os.getpid()))
+
         scheduler = get_scheduler(
             name=options.get('queue'), interval=options.get('interval'))
         scheduler.run()
