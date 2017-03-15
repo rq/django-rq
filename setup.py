1b4d61046e109c266163bd3e258549e8777e73e5
Selwin Ong <selwin.ong@gmail.com>
2017-03-15 19:19:08 +0700

    Updated Python 3.5 compatibility classifier in setup.py.

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
