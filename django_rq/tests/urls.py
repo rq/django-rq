7758a963b7fe1811afa02e111a5044780e77f197
Selwin Ong <selwin.ong@gmail.com>
2017-12-06 14:03:23 +0700

    Django 2.0 compatibility (#267)
    * Fixes test imports.
    
    * Fixed test errors.
    
    * Fixed tests for Django < 2.0.

--- django_rq/tests/urls.py
+++ django_rq/tests/urls.py
@@ -1,11 +1,20 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.core.exceptions import ImproperlyConfigured
 
 from django_rq.tests import views
+from django_rq.urls import urlpatterns
 
 urlpatterns = [
-    url(r'^django-rq/', include('django_rq.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^django-rq/', (urlpatterns, '', 'django_rq')),
+    url(r'^admin/', admin.site.urls),
     url(r'^success/$', views.success, name='success'),
     url(r'^error/$', views.error, name='error'),
 ]
+
+try:
+    # For Django < 2.0
+    urlpatterns += [url(r'^django-rq/', include('django_rq.urls'))]
+except ImproperlyConfigured:
+    # Django >= 2.0 URL dispatcher syntax
+    urlpatterns += [url(r'^django-rq/', (urlpatterns, '', 'django_rq'))]
