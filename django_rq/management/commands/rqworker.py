16c0e33c9b752cafc6f96d93b5a9c1f7548becc1
Hugo Rodger-Brown <hugorodgerbrown@users.noreply.github.com>
2020-09-25 00:55:54 +0100

    Update rqworker command Sentry configuration (alternative version) (#442)
    * Update rqworker command to incorporate existing sentry config
    
    I have added a method to the command to combine existing Sentry config
    options with the options passed in on the command invocation. This means
    that if you have initialised Sentry in the Django settings module with specific
    options, these will be maintained.
    
    Fixes #440
    
    * Add test for sentry options
    
    * Add tests for rqworker Command.sentry_options method
    
    * Bump up coverage on tests to get through codecov
    
    * Bump test coverage
    
    * Add coverage to dev-packages
    
    * Refactor previous implementation (WIP)
    
    * Refactor to simplify sentry config
    
    * Fix failing tests
    
    * Remove duplicate integrations key from sentry options
    
    * Refactor sentry configuration
    
    In the version I have attempted to tackle the issue of Sentry
    configuration from a clean slate. I have removed the code in
    rqworker that falls back the the `SENTRY_DSN` Django
    setting, on the basis that if the client user has set this in
    their project settings.py, then they are capable of configuring
    Sentry there.
    
    If the user passes `sentry_dsn` explicitly to the command,
    then this will override any configuration in the Django
    settings, and replace them with a configuration that includes
    the RqIntegration and DjangoIntegration.
    
    * Add RedisIntegration to Sentry configuration

--- django_rq/management/commands/rqworker.py
+++ django_rq/management/commands/rqworker.py
@@ -6,7 +6,6 @@ from redis.exceptions import ConnectionError
 from rq import use_connection
 from rq.logutils import setup_loghandlers
 
-from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import connections
 from django.utils.version import get_version
@@ -17,6 +16,34 @@ from ...workers import get_worker
 def reset_db_connections():
     for c in connections.all():
         c.close()
+
+
+def configure_sentry(sentry_dsn, **options):
+    """
+    Configure the Sentry client.
+
+    The **options kwargs are passed straight from the command
+    invocation - options relevant to Sentry configuration are
+    extracted.
+
+    In addition to the 'debug' and 'ca_certs' options, which can
+    be passed in as command options, we add the RqIntegration and
+    DjangoIntegration to the config.
+
+    Raises ImportError if the sentry_sdk is not available.
+
+    """
+    import sentry_sdk
+    sentry_options = {
+        'debug': options.get('sentry_debug', False),
+        'ca_certs': options.get('sentry_ca_certs', None),
+        'integrations': [
+            sentry_sdk.integrations.redis.RedisIntegration(),
+            sentry_sdk.integrations.rq.RqIntegration(),
+            sentry_sdk.integrations.django.DjangoIntegration()
+        ]
+    }
+    sentry_sdk.init(sentry_dsn, **sentry_options)
 
 
 class Command(BaseCommand):
@@ -48,11 +75,11 @@ class Command(BaseCommand):
         parser.add_argument('--worker-ttl', action='store', type=int,
                             dest='worker_ttl', default=420,
                             help='Default worker timeout to be used')
-        parser.add_argument('--sentry-dsn', action='store', default=None, dest='sentry-dsn',
+        parser.add_argument('--sentry-dsn', action='store', default=None, dest='sentry_dsn',
                             help='Report exceptions to this Sentry DSN')
-        parser.add_argument('--sentry-ca-certs', action='store', default=None, dest='sentry-ca-certs',
+        parser.add_argument('--sentry-ca-certs', action='store', default=None, dest='sentry_ca_certs',
                             help='A path to an alternative CA bundle file in PEM-format')
-        parser.add_argument('--sentry-debug', action='store', default=False, dest='sentry-debug',
+        parser.add_argument('--sentry-debug', action='store', default=False, dest='sentry_debug',
                             help='Turns debug mode on or off.')
 
         if LooseVersion(get_version()) >= LooseVersion('1.10'):
@@ -64,9 +91,6 @@ class Command(BaseCommand):
         if pid:
             with open(os.path.expanduser(pid), "w") as fp:
                 fp.write(str(os.getpid()))
-        sentry_dsn = options.get('sentry-dsn')
-        if sentry_dsn is None:
-            sentry_dsn = getattr(settings, 'SENTRY_DSN', None)
 
         # Verbosity is defined by default in BaseCommand for all commands
         verbosity = options.get('verbosity')
@@ -77,6 +101,14 @@ class Command(BaseCommand):
         else:
             level = 'INFO'
         setup_loghandlers(level)
+
+        sentry_dsn = options.pop('sentry_dsn')
+        if sentry_dsn:
+            try:
+                configure_sentry(sentry_dsn, **options)
+            except ImportError:
+                self.stderr.write("Please install sentry-sdk using `pip install sentry-sdk`")
+                sys.exit(1)
 
         try:
             # Instantiate a worker
@@ -95,26 +127,7 @@ class Command(BaseCommand):
             # Close any opened DB connection before any fork
             reset_db_connections()
 
-            if sentry_dsn:
-                sentry_debug = options.get('sentry-debug') or getattr(
-                    settings, 'SENTRY_DEBUG', False
-                )
-                sentry_options = {'debug': sentry_debug}
-
-                sentry_ca_certs = options.get('sentry-ca-certs') or getattr(
-                    settings, 'SENTRY_CA_CERTS', None
-                )
-                if sentry_ca_certs:
-                    sentry_options.update({'ca_certs': sentry_ca_certs})
-
-                try:
-                    from rq.contrib.sentry import register_sentry
-                    register_sentry(sentry_dsn, **sentry_options)
-                except ImportError:
-                    self.stdout.write(self.style.ERROR("Please install sentry-sdk using `pip install sentry-sdk`"))
-                    sys.exit(1)
-
             w.work(burst=options.get('burst', False), with_scheduler=options.get('with_scheduler', False), logging_level=level)
         except ConnectionError as e:
-            print(e)
+            self.stderr.write(str(e))
             sys.exit(1)
