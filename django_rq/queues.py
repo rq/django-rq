5e67c47df78938354c79a18032015470f6c2b345
Juan Jose Garcia Alvite <juanjgarcia@users.noreply.github.com>
2021-05-07 05:38:56 +0100

    Add ability to pass arbitrary Redis client kwargs (#482)

--- django_rq/queues.py
+++ django_rq/queues.py
@@ -112,7 +112,16 @@ def get_redis_connection(config, use_strict_redis=False):
             service_name=config['MASTER_NAME'], redis_class=redis_cls,
         )
 
-    return redis_cls(host=config['HOST'], port=config['PORT'], db=config.get('DB', 0), username=config.get('USERNAME', None), password=config.get('PASSWORD'), ssl=config.get('SSL', False), ssl_cert_reqs=config.get('SSL_CERT_REQS', 'required'))
+    return redis_cls(
+        host=config['HOST'],
+        port=config['PORT'],
+        db=config.get('DB', 0),
+        username=config.get('USERNAME', None),
+        password=config.get('PASSWORD'),
+        ssl=config.get('SSL', False),
+        ssl_cert_reqs=config.get('SSL_CERT_REQS', 'required'),
+        **config.get('REDIS_CLIENT_KWARGS', {})
+    )
 
 
 def get_connection(name='default', use_strict_redis=False):
