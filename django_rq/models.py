142fc140e8c6b0d0ab8d882a9c48dc8834d422cb
Håken Lid <haakenlid@gmail.com>
2021-09-28 02:29:42 +0200

    Use ModelAdmin class to enable sidebar and permission in django admin (#512)

--- django_rq/models.py
+++ django_rq/models.py
@@ -1,10 +1,20 @@
 from django.core.signals import got_request_exception, request_finished
+from django.db import models
 
 from . import thread_queue
 from .queues import get_commit_mode
-
 
 # If we're not in AUTOCOMMIT mode, wire up request finished/exception signal
 if not get_commit_mode():
     request_finished.connect(thread_queue.commit)
     got_request_exception.connect(thread_queue.clear)
+
+
+class Queue(models.Model):
+    """Placeholder model with no database table, but with django admin page
+    and contenttype permission"""
+
+    class Meta:
+        managed = False  # not in Django's database
+        default_permissions = ()
+        permissions = [['view', 'Access admin page']]
