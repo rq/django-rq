9f6c30e55153d7e85316e6528a47399cee751d93
Gabriel Y. Salem <lazaro.salem@dxc.com>
2023-02-14 21:23:58 -0500

    Remove unnecesary code

--- django_rq/models.py
+++ django_rq/models.py
@@ -13,8 +13,6 @@ if not get_commit_mode():
 class Queue(models.Model):
     """Placeholder model with no database table, but with django admin page
     and contenttype permission"""
-    id = models.AutoField(primary_key=True)
-
     class Meta:
         managed = False  # not in Django's database
         default_permissions = ()
