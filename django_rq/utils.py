e3fefdbccd1d4577dcc9e781788a1509781f883a
Roman Donchenko <rdonchen@outlook.com>
2024-05-01 10:24:33 +0300

    Handle the case where the Connection to Redis uses Sentinels
    Currently, when PyYAML is not installed, `rqstats -y` prints an error
    message, but it still exits with a 0 exit code, indicating success. Fix
    that, along with a few related issues:
    
    * The error message was printed to stdout rather than stderr.
    
    * The name of the library in the message was incorrect (LibYAML is a C
      library; the Python library is PyYAML).
    
    After this change, you need to install PyYAML to run the testsuite, so
    modify the GitHub workflow accordingly. Also, make sure to install all
    packages at once, to make sure that later install commands don't overwrite
    the result of the earlier ones.

--- django_rq/utils.py
+++ django_rq/utils.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ImproperlyConfigured
 from django.db import connections
+from redis.sentinel import SentinelConnectionPool
 from rq.command import send_stop_job_command
 from rq.job import Job
 from rq.registry import (
@@ -102,7 +103,11 @@ def get_scheduler_statistics():
         # to handle the possibility of a configuration with multiple redis connections and scheduled
         # jobs in more than one of them
         queue = get_queue_by_index(index)
-        connection = queue.connection.connection_pool.connection_kwargs
+        if isinstance(queue.connection.connection_pool, SentinelConnectionPool):
+            first_sentinel = queue.connection.connection_pool.sentinel_manager.sentinels[0]
+            connection = first_sentinel.connection_pool.connection_kwargs
+        else:
+            connection = queue.connection.connection_pool.connection_kwargs
         conn_key = f"{connection['host']}:{connection.get('port', 6379)}/{connection.get('db', 0)}"
         if conn_key not in schedulers:
             try:
