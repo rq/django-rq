c4adadea37e0cc33bb870c31c4a4d14cbb3eaaaf
Selwin Ong <selwin.ong@gmail.com>
2024-10-28 22:39:40 +0700

    Bump version to 3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-rq',
-    version='2.10.2',
+    version='3.0.0',
     author='Selwin Ong',
     author_email='selwin.ong@gmail.com',
     packages=['django_rq'],
