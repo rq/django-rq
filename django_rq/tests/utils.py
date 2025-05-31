1c23862d6bbadbc92a0f1cd2e42c64abd77f9ebe
Terence Honles <terence@honles.com>
2025-05-31 10:21:11 +0200

    fix JSON stats view and tests with redis-py 6.0 (#706)
    x-ref: https://github.com/redis/redis-py/pull/3622

--- django_rq/tests/utils.py
+++ django_rq/tests/utils.py
@@ -5,15 +5,18 @@ def get_queue_index(name='default'):
     """
     Returns the position of Queue for the named queue in QUEUES_LIST
     """
-    queue_index = None
     connection = get_connection(name)
     connection_kwargs = connection.connection_pool.connection_kwargs
+
     for i in range(0, 100):
         try:
             q = get_queue_by_index(i)
         except AttributeError:
             continue
-        if q.name == name and q.connection.connection_pool.connection_kwargs == connection_kwargs:
-            queue_index = i
-            break
-    return queue_index
+        if q.name == name:
+            # assert that the connection is correct
+            assert q.connection.connection_pool.connection_kwargs == connection_kwargs
+
+            return i
+
+    return None
